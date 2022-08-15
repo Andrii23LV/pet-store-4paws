c391684c837ca6c44fa574bac9a17da231e3d027
Andrii23LV <tretiak.andrii.lviv@gmail.com>
2022-08-15 11:59:34 +0300

    Bugs fixed, components are sorted in folders

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,10 +22,10 @@ export function Navigation() {
         <h2 className="nav-text">FourPaws</h2>
       </div>
       <span className='nav-elements'>
-        <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>Products</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>About us</NavLink>
-        <NavLink to="/orders" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>Orders{collarSum > 0 ? <span id='orders-num'>{collarSum}</span> : ''}</NavLink>
-        <NavLink to="/account" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>{isLogged ? username : 'Account'}</NavLink>
+        <NavLink to="/pet-store-4paws/products" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>Products</NavLink>
+        <NavLink to="/pet-store-4paws/about" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>About us</NavLink>
+        <NavLink to="/pet-store-4paws/orders" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>Orders{collarSum > 0 ? <span id='orders-num'>{collarSum}</span> : ''}</NavLink>
+        <NavLink to="/pet-store-4paws/account" className={({ isActive }) => (isActive ? 'nav-element-active' : 'nav-element')}>{isLogged ? username : 'Account'}</NavLink>
       </span>
     </nav>
   )
